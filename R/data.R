22caff0dc131d6c89f0ef058650ddbeeccccfbcd
José C. Pernías <jcpernias@gmail.com>
2020-02-18 13:23:25 +0100

    Docuement ecv2018 dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Data from the Encuesta de Condiciones de Vida
+#'
+#' A small subset of the information contained on the 2018 edition
+#' of the Encuesta de Condiciones de Vida.
+#'
+#' @format A tibble with 13368 observations and 24 variables:
+#' \describe{
+#'   \item{ydisp_hh}{annual disposable income, euros.}
+#'   \item{cu}{consumption units, modified OECD equivalence scale.}
+#' }
+#' @source \href{https://ine.es/dyngs/INEbase/es/operacion.htm?c=Estadistica_C&cid=1254736176807&menu=resultados&secc=1254736195153&idp=1254735976608}{Microdata at INE, the Spanish Statistical Office}
+'ecv2018'
