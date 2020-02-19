0f656a9e5b46e73e1d371821cefd2f70ab57ea05
José C. Pernías <jcpernias@gmail.com>
2020-02-19 10:36:01 +0100

    Clean Gini index code

--- R/gini.R
+++ R/gini.R
@@ -12,13 +12,16 @@ gini <- function(y, w) {
   n <- length(y)
   oidx <- order(y)
   yo <- y[oidx]
-  # TODO: check this hack
+
   if (missing(w)) {
-    wo <- rep_len(1 / n, length.out = n)
+    x <- (1:n) / n
+    yo_mean <- mean(yo)
+    C <- mean((yo - yo_mean) * x)
   } else {
     wo <- w[oidx] / sum(w)
+    x <- wo / 2 + c(0, cumsum(wo)[-n])
+    yo_mean <- sum(yo * wo)
+    C <- sum(wo * (yo - yo_mean) * x)
   }
-  x <- wo / 2 + c(0, cumsum(wo)[-n])
-  yo_mean <- sum(yo * wo)
-  2 * sum(wo * (yo - yo_mean) * x) / yo_mean
+  2 * C / yo_mean
 }
