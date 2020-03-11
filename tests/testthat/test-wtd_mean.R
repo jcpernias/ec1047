24271bf2dda81032a9c12718f00bdb7ddabfe623
José C. Pernías <jcpernias@gmail.com>
2020-03-11 17:58:27 +0100

    Drop na.rm argument

--- tests/testthat/test-wtd_mean.R
+++ tests/testthat/test-wtd_mean.R
@@ -25,13 +25,24 @@ test_that("NA values in x", {
   x <- c(NA, 2, 3, 4, 5)
   w <- c(5, 4, 3, 2, 1)
   expect_true(is.na(wtd_mean(x, weights = w)))
-  expect_equal(wtd_mean(x, weights = w, na.rm = TRUE),
-               stats::weighted.mean(x, w, na.rm = TRUE))
 })
 
 test_that("NA values in w", {
   x <- c(1, 2, 3, 4, 5)
   w <- c(NA, 4, 3, 2, 1)
   expect_true(is.na(wtd_mean(x, weights = w)))
-  expect_true(is.na(wtd_mean(x, weights = w, na.rm = TRUE)))
+})
+
+test_that("zero weights", {
+  x <- c(NA, 2, 3, 4, 5)
+  w <- c(0, 4, 3, 2, 1)
+  expect_equal(wtd_mean(x, weights = w), wtd_mean(x[-1], w[-1]))
+  expect_true(is.na(wtd_mean(x, weights = rep(0, times = 5))))
+})
+
+test_that("zero length inputs", {
+  x <- numeric()
+  w <- numeric()
+  expect_true(is.na(wtd_mean(x)))
+  expect_true(is.na(wtd_mean(x, weights = w)))
 })
