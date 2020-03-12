06c8e5ad4c372e1902249acc0cf7b47748b2785b
José C. Pernías <jcpernias@gmail.com>
2020-03-12 17:48:54 +0100

    Remove unnecessary checks

--- R/wtd_mean.R
+++ R/wtd_mean.R
@@ -3,8 +3,8 @@
 #' Compute the mean of \code{x} weighted by \code{weights}.
 #'
 #' If \code{weights} is \code{NULL}, all elements of \code{x} receive the
-#' same weight. Observations with zero weights are omitted from the
-#' computation. Missing values are not handled specially and
+#' same weight. Observations with zero weights are omitted before the mean
+#' is computed. Missing values are not handled specially and
 #' produce a missing value as the result.
 #'
 #' @param x a numerical vector.
@@ -19,24 +19,25 @@
 #' @export
 wtd_mean <- function(x, weights = NULL) {
   checkmate::qassert(x, 'n')
-  if (!length(x))
-    return(NA_real_)
-  if (checkmate::qtest(weights, '0'))
-    return(sum(x) / length(x))
-
-
-  checkmate::assert_numeric(weights, len = length(x))
-  if(checkmate::anyMissing(weights))
+  n_x <- length(x)
+  if (n_x == 0)
     return(NA_real_)
 
-  idx <- weights != 0
-  if (any(!idx)) {
-    x <- x[idx]
-    weights <- weights[idx]
-    if (!length(x))
-      return(NA_real_)
+  if (is.null(weights))
+    return(sum(x) / n_x)
+
+  checkmate::assert_numeric(weights, len = n_x)
+  if (checkmate::anyMissing(weights))
+    return(NA_real_)
+
+  nonzero_weights <- weights != 0
+  if (!any(nonzero_weights))
+    return(NA_real_)
+
+  if (!all(nonzero_weights)) {
+    x <- x[nonzero_weights]
+    weights <- weights[nonzero_weights]
   }
 
-  weights <- weights / sum(weights)
-  sum(weights * x)
+  sum(weights * x / sum(weights))
 }
